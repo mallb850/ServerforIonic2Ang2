bba97e5f510a3f7c7d141f21c9452b5568d2bc34
Jamaal <jb91@students.uwf.edu>
2017-05-19 13:49:33 -0500

    wip

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var cloudconfig = require('../server/config/cloudinary.json');
 mongoose.connect(config.uri);
 
 
-app.all('/', function(req, res, next) {
+app.all(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
