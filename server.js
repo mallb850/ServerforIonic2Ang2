d50d5c769110527f17efd021ef99e36ba2131f5e
Jamaal <jb91@students.uwf.edu>
2017-05-19 14:53:24 -0500

    wip

--- server.js
+++ server.js
@@ -25,6 +25,16 @@ var cloudconfig = require('../server/config/cloudinary.json');
 
 mongoose.connect(config.uri);
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+app.use(methodOverride());
+app.use(cors());
+app.use(passport.initialize());
+
+
+
 
 app.all(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -33,13 +43,6 @@ app.all(function(req, res, next) {
   next();
  });
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
-app.use(methodOverride());
-app.use(cors());
-app.use(passport.initialize());
 
 
 cloudinary.config({ 
