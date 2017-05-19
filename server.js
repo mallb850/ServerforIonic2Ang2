273921d6ef904285f83aa384032c7a7d45bf7ebe
Jamaal <jb91@students.uwf.edu>
2017-05-19 18:37:11 -0500

    wip

--- server.js
+++ server.js
@@ -25,6 +25,15 @@ var cloudconfig = require('../server/config/cloudinary.json');
 
 mongoose.connect(config.uri);
 
+app.all('*', function (req, res, next) {
+  var origin = req.get('origin');
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
+
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -35,13 +44,6 @@ app.use(passport.initialize());
 
 
 
-
-app.all(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
-  next();
- });
 
 
 
